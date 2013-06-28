51d7fa94072df5493600da7cd482753fef842d8f
Eymen Gunay <eymen@egunay.com>
2013-06-28 14:25:16 +0200

    Kernel updated for easier extend

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -11,31 +11,52 @@ use Symfony\Component\Config\Loader\LoaderInterface;
  
 class AppKernel extends Kernel
 {
+    /**
+     * {@inheritdoc}
+     */
     public function registerBundles()
     {   
-        return array(
+        $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
         );
+        
+        // This small hack allows to register custom 
+        // bundles required for executing tests. Make sure
+        // that registerBundles function returns an array.
+        if (function_exists('registerBundles')) {
+            $bundles = array_merge($bundles, registerBundles());
+        }
+
+        return $bundles;
     }   
  
+    /**
+     * {@inheritdoc}
+     */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {   
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+
+        // This small hack allows to register custom 
+        // configuration files for executing tests.
+        if (function_exists('registerContainerConfiguration')) {
+            registerContainerConfiguration($loader);
+        }
     }   
  
-    /** 
-     * @return string
+    /**
+     * {@inheritdoc}
      */
     public function getCacheDir()
     {   
-        return sys_get_temp_dir().'/EoPassbookBundle/cache';
+        return sys_get_temp_dir().'/SymfonyTestEdition/cache';
     }   
  
-    /** 
-     * @return string
+    /**
+     * {@inheritdoc}
      */
     public function getLogDir()
     {   
-        return sys_get_temp_dir().'/EoPassbookBundle/logs';
+        return sys_get_temp_dir().'/SymfonyTestEdition/logs';
     }   
 }
